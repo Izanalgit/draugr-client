fb9886fd0edab4059a64804c4346b33b8daca92b
Izanalgit <izanalvarez@gmail.com>
2025-01-29 17:09:22 +0100

    feat : countdown timer component

---
+++ src/components/CountdownTimer.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { useApp } from "../context/AppContext";
+
+const CountdownTimer = () => {
+
+    const { certifiedTime, timeLimit } = useApp();
+
+    const [timeLeft, setTimeLeft] = useState(timeLimit);
+
+    useEffect(() => {
+        if(certifiedTime){
+            const updateRemainingTime = () => {
+                const elapsed = Date.now() - certifiedTime;
+                setTimeLeft(Math.max(timeLimit - elapsed, 0)); // Avoid 0
+            };
+
+            const interval = setInterval(updateRemainingTime, 1000); // Each 1 sec
+
+            updateRemainingTime();
+
+            return () => clearInterval(interval);
+        }
+
+    }, [certifiedTime]);
+
+    // Format time
+    const formatTime = (ms) => {
+        const minutes = Math.floor(ms / 60000); // mins 
+        const seconds = Math.floor((ms % 60000) / 1000); // secs
+
+        return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+    };
+
+    return (
+        <div>
+            {timeLeft > 0 ? (
+                <span>{formatTime(timeLeft)}</span>
+            ) : (
+                <span>EXPIRADO</span>
+            )}
+        </div>
+    );
+};
+
+export default CountdownTimer;
