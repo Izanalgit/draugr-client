85bc26b460d0bde0c91e1db717ad8d1eb3520a3b
Izanalgit <izanalvarez@gmail.com>
2025-01-29 17:03:27 +0100

    refact : routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-// import Chat from "../pages/Chat.jsx";
-// import Invite from "../pages/Invite.jsx";
+import Chat from "../pages/Chat.jsx";
 import Loader from "../pages/Loader.jsx";
-// import Request from "../pages/Request.jsx";
-// import Response from "../pages/Response.jsx";
+import Response from "../pages/Response.jsx";
 
 const AppRoutes = () =>{
     return(
         <Router>
             <Routes>
                 <Route path="/" element={<Loader />} />
-                {/* <Route path="/invite" element={<Invite />} />
-                <Route path="/request" element={<Request />} />
+                <Route path="/chat" element={<Chat />} />
                 <Route path="/response" element={<Response />} />
-                <Route path="/chat/:chatToken" element={<Chat />} /> */}
             </Routes>
         </Router>
     );
