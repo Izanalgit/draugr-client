979f21b8debc3dad8ae72e6f85f4227483d63e21
Izanalgit <izanalvarez@gmail.com>
2025-01-31 21:30:44 +0100

    feat : landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import LandingComponent from "../components/LandingComponent";
+
+const Landing = () => {
+
+    return(
+        <>
+            <h1>CHAT DRAUGR</h1>
+            <LandingComponent />
+        </>
+    )
+}
+
+export default Landing;
