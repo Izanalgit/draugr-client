830fcc6998c948c6423df2c2ee118ac35f581c90
Izanalgit <izanalvarez@gmail.com>
2025-01-31 21:31:08 +0100

    feat : landing page on routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Chat from "../pages/Chat.jsx";
+import Landing from "../pages/Landing.jsx";
 import Loader from "../pages/Loader.jsx";
 import Response from "../pages/Response.jsx";
 
@@ -8,7 +9,8 @@ const AppRoutes = () =>{
     return(
         <Router>
             <Routes>
-                <Route path="/" element={<Loader />} />
+                <Route path="/" element={<Landing />} />
+                <Route path="/loader" element={<Loader />} />
                 <Route path="/chat" element={<Chat />} />
                 <Route path="/response" element={<Response />} />
             </Routes>
